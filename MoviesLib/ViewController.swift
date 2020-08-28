1712d4c1704248628264136cc1979eb5e75d2728
Eric Brito <eric_brito@msn.com>
2020-08-27 21:58:22 -0300

    - Finalização do AutoLayout da tela de visualização de filme

--- MoviesLib/ViewController.swift
+++ MoviesLib/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // TODO: Remover a variante da imagem
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
