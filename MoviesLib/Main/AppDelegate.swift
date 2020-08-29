5e9797d6e5f0767a0fd48eb456942c134a079268
Eric Brito <eric_brito@msn.com>
2020-08-28 22:17:04 -0300

    - Primeira aula de JSON e TableView

---
+++ MoviesLib/Main/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MoviesLib
+//
+//  Created by Eric Alves Brito on 26/08/20.
+//  Copyright © 2020 DevBoost. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
