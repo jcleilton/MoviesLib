c3be7d5ae704d5686f9bf842af5e8e8d91b7633f
Eric Brito <eric_brito@msn.com>
2020-09-03 23:56:45 -0300

    - UserDefaults, Settings Bundle e Multimídia

--- MoviesLib/Main/AppDelegate.swift
+++ MoviesLib/Main/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
 
     // MARK: - Core Data stack
 
---
+++ MoviesLib/MovieVisualization/Models/ItunesResult.swift
@@ -0,0 +1,13 @@
+//
+//  ItunesResult.swift
+//  MoviesLib
+//
+//  Created by Eric Alves Brito on 03/09/20.
+//  Copyright © 2020 DevBoost. All rights reserved.
+//
+
+import Foundation
+
+struct ItunesResult: Codable {
+    let results: [MovieInfo]
+}
